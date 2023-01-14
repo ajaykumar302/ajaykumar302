ad6e8f0b13355261584de97e11f8042c1b62f535
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2023-01-14 11:05:21 +0530

    file

---
+++ index.js
@@ -0,0 +1,19 @@
+const arr = [4,5,6,7,4,8,9,7]
+// ​ If I got 2 JS Files, var a = 10; //script.1js, if we import file 1 and File 2, will the global file will also move?
+
+// nodemailer
+
+// express validator
+//pattern
+// write a function to print the factorial of 5 using recursion?
+// write a function to reverse the string without using js methods?
+
+// How will you refresh a page after a specific seconds automatically ?
+// Write a program to merge two arrays using while loop?
+// what is nullish coalescing operator?
+// What is Inheritance?
+// What are class access modifiers?
+// What is method overriding?
+// What is overloading? Why and when do we use it?
+// Explain public, private and protected data.
+// What is event bubbling and event capturing
